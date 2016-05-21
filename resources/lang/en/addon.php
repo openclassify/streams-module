8bf3182612bffad17b677d9d54ae913e5ac58813
Ryan Thompson <ryan@pyrocms.com>
2016-05-20 23:51:19 -0500

    Initial commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Streams',
+    'name'        => 'Streams Module',
+    'description' => ''
+];
---
+++ src/StreamsModule.php
@@ -0,0 +1,24 @@
+<?php namespace Anomaly\StreamsModule;
+
+use Anomaly\Streams\Platform\Addon\Module\Module;
+
+class StreamsModule extends Module
+{
+
+    /**
+     * The navigation icon.
+     *
+     * @var string
+     */
+    protected $icon = 'addon';
+
+    /**
+     * The module sections.
+     *
+     * @var array
+     */
+    protected $sections = [
+        'example'
+    ];
+
+}
