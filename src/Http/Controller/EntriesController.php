9e35097580428c28a167fe04e2ccadbcc5c039b6
Ryan Thompson <service@aiwebsystems.com>
2019-01-16 02:49:22 -0600

    Tweek

--- src/Http/Controller/EntriesController.php
+++ src/Http/Controller/EntriesController.php
@@ -2,6 +2,7 @@
 
 use Anomaly\EditorFieldType\EditorFieldTypePresenter;
 use Anomaly\Streams\Platform\Http\Controller\PublicController;
+use Anomaly\Streams\Platform\View\ViewTemplate;
 use Anomaly\StreamsModule\Configuration\Contract\ConfigurationInterface;
 use Anomaly\StreamsModule\Configuration\Contract\ConfigurationRepositoryInterface;
 
@@ -19,48 +20,46 @@ class EntriesController extends PublicController
      * List entries in the stream.
      *
      * @param ConfigurationRepositoryInterface $configurations
+     * @param ViewTemplate $template
      * @return \Illuminate\Http\Response
      */
-    public function index(ConfigurationRepositoryInterface $configurations)
+    public function index(ConfigurationRepositoryInterface $configurations, ViewTemplate $template)
     {
         /* @var ConfigurationInterface $configuration */
         if (!$configuration = $configurations->find($this->route->getAction('anomaly.module.streams::configuration'))) {
             abort(404);
         }
 
+        // Pass the stream to the template globals.
+        $template->set('stream', $stream = $configuration->getRelated());
+
         /* @var EditorFieldTypePresenter $template */
         $template = $configuration->getFieldTypePresenter('index_template');
 
-        return $this->response->view(
-            $template->path(),
-            [
-                'stream' => $configuration->getRelated(),
-            ]
-        );
+        return $this->response->view($template->path(), compact('stream'));
     }
 
     /**
      * View an entry in the stream.
      *
      * @param ConfigurationRepositoryInterface $configurations
+     * @param ViewTemplate $template
      * @return \Illuminate\Http\Response
      */
-    public function view(ConfigurationRepositoryInterface $configurations)
+    public function view(ConfigurationRepositoryInterface $configurations, ViewTemplate $template)
     {
         /* @var ConfigurationInterface $configuration */
         if (!$configuration = $configurations->find($this->route->getAction('anomaly.module.streams::configuration'))) {
             abort(404);
         }
 
+        // Pass the stream to the template globals.
+        $template->set('stream', $stream = $configuration->getRelated());
+
         /* @var EditorFieldTypePresenter $template */
         $template = $configuration->getFieldTypePresenter('view_template');
 
-        return $this->response->view(
-            $template->path(),
-            [
-                'stream' => $configuration->getRelated(),
-            ]
-        );
+        return $this->response->view($template->path(), compact('stream'));
     }
 
 }
